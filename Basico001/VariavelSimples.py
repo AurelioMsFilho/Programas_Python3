b75f4632afef8168d01a9f3b4bdfc673ce187373
AurelioMsFilho <Aureliofilhotj@gmail.com>
2022-08-28 14:50:07 -0300

    variavel simples

---
+++ Basico001/VariavelSimples.py
@@ -0,0 +1,18 @@
+"""Escreva um programa separado para resolver cada um destes exercícios. Salve
+cada programa com um nome de arquivo que siga as convenções-padrão de
+Python, com letras minúsculas e underscores, por exemplo, simple_message.py e
+simple_messages.py.
+2.1 – Mensagem simples: Armazene uma mensagem em uma variável e, em
+seguida, exiba essa mensagem.
+2.2 – Mensagens simples: Armazene uma mensagem em uma variável e, em
+seguida, exiba essa mensagem. Então altere o valor de sua variável para uma
+nova mensagem e mostre essa nova mensagem."""
+
+message = 'Amo Praia'
+print(message)
+
+message2 = 'tenho 4 dogs'
+
+print(message2)
+message2 = 'e mais um gato'
+print(message2.count('m'))
